d2079aa780d3c99c000f30b20eb20c828fb014d3
Ariel Gentile <67652668+ariel151515@users.noreply.github.com>
2023-02-15 15:44:08 -0300

    ww

---
+++ tipos de datos/tiposdedatos.py
@@ -0,0 +1,40 @@
+"string"  # string
+
+# como los template string en javascript
+""" tus datos son:   
+    nombre:licas
+    apellido: dalto"""
+
+40   # INT    (entero)
+40.2  # FLOAT (flotante)
+False  # BOOL (los boleanos van con mayuscula)
+True
+
+# Variables
+nombre = "Ariel"
+nombre = "Juan"
+
+numero = 10
+numero += 1  # igual anumero + 1
+numero += 1  # igual numero - 1
+print(numero)
+
+
+# Concatenar fstring (del borra nombre)
+nombre = "Lucas"
+bienvenida = f"Hola {nombre} ¿Como estas?"
+print(bienvenida)
+
+# del para hacer que una variable no este mas declarada
+del bienvenida
+
+# Operadores de pertenencia   (in / not in)
+print('Lucas' in bienvenida)  # true
+print('Lucas' not in bienvenida)  # false
+
+
+# definir vaiable con camelCase ()el principio de cada nueva palabra va con mayuscula
+nombreCompletoDeTuTioMaster = "Lucas Dalto"
+
+# definir vaiable con snake_case
+nombre_completo_de_tu_tio_master = "Lucas Dalto"
